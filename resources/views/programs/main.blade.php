4a0c477866b1d7cb22167bd8e06e9c53bc3bf62b
chamodya <chamodyawimansha@gmail.com>
2019-03-25 08:24:29 +0530

    bedore Vue intergration.. everything works file

--- resources/views/programs/main.blade.php
+++ resources/views/programs/main.blade.php
@@ -16,7 +16,35 @@
         </div>
         @endif
 
-        You are logged in!
+
+
+          <div class="table-responsive table-full-width">
+            <table class="table table-hover">
+              <thead>
+                <th>#</th>
+                <th class="w-50">Program Title</th>
+                <th>Start Date</th>
+                <th>Closing Date</th>
+                <th>No.of Trainees</th>
+                <th>Attending</th>
+              </thead>
+              <tbody>
+              @for($i=0; $i<20; $i++)
+                <tr>
+                  <td>{{$i}}</td>
+                  <td><a href="" >Dakota Rice</a></td>
+                  <td>$36,738</td>
+                  <td>Niger</td>
+                  <td>Oud-Turnhout</td>
+                  <td>Oud-Turnhout</td>
+                </tr>
+              @endfor
+              </tbody>
+            </table>
+          </div>
+
+
+
       </div>
     </div>
   </div>
