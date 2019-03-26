8d70c1d12b51a711207a7f45ad7a9791ff6f7280
chamodya <chamodyawimansha@gmail.com>
2019-03-26 23:01:46 +0530

    new foreign program form error classes changed

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@
 
 Auth::routes();
 
+
 Route::get('/', 'HomeController@index')->name('home')->middleware('auth');
 
 Route::get('/programs', 'ProgramController@index')->name('program')->middleware('auth');
@@ -22,8 +23,14 @@ Route::get('/trainees', 'TraineeController@index')->name('Trainee')->middleware(
 Route::get('/programs/local', 'LocalProgramController@index')->name('programs/local')->middleware('auth');
 
 Route::POST('/programs/local', 'LocalProgramController@store')->name('programs/local')->middleware('auth');
-
+/**
+ * Show the Foreign Training Program Form
+ */
 Route::get('/programs/foreign', 'ForeignController@index')->name('programs/foreign')->middleware('auth');
+/**
+ * Handle the Foreign Training Program Form submition request
+ */
+Route::POST('/programs/foreign', 'ForeignController@store')->name('programs/foreign')->middleware('auth');
 
 Route::get('/programs/inhouse', 'InHouseProgramController@index')->name('programs/inhouse')->middleware('auth');
 
