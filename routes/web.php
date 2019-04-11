8786abb6b4dd133b361ffc3c51b0ec4b09b00461
chamodya <chamodyawimansha@gmail.com>
2019-04-11 06:55:08 +0530

    before pull

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
