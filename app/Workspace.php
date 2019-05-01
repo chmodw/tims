ad51ae99bcbb10e554a10821cb1ca215b1fc67b9
bellhill39 <playyboyy39@gmail.com>
2019-05-02 01:22:48 +0530

    Update Workspace.php

--- app/Workspace.php
+++ app/Workspace.php
@@ -4,19 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Workspace extends Model
+class WorkSpace extends Model
 {
     //Db name
     protected $connection = 'sql_get';
     //Table name
     protected $table = 'dbo.cmn_WorkSpace';
-
-
-    public function WorkSpaceTypeId()
-    {
-        return $this->belongsTo('App\WorkSpaceType');
-    }
-
-
-
 }
